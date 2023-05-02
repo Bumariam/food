a403026e1169fdcc44d3d5833198f685daf130ed
Bumariam Nakeeva <bumariamnakeeva@gmail.com>
2023-05-02 16:57:11 +0600

    food2

--- myapp/views.py
+++ myapp/views.py
@@ -2,43 +2,53 @@ from django.shortcuts import render, get_object_or_404
 from .models import Restaurant, FastFood, Caffeine, MenuItem
 
 
-
 def home(request):
     caffeine_list = Caffeine.objects.all()
     fastfood_list = FastFood.objects.all()
     restaurant_list = Restaurant.objects.all()
+    menu_items = MenuItem.objects.all()
     context = {
         'caffeine_list': caffeine_list,
         'fastfood_list': fastfood_list,
         'restaurant_list': restaurant_list,
+        'menu_items': menu_items,
     }
     return render(request, 'home.html', context)
 
 
 def restaurant_list(request):
-    restaurants = Restaurant.objects.all()
-    context = {'restaurants': restaurants}
+    restaurant = Restaurant.objects.all()
+    context = {'restaurant': restaurant}
     return render(request, 'restaurant_list.html', context)
+
 
 def restaurant_detail(request, pk):
     restaurant = get_object_or_404(Restaurant, pk=pk)
     return render(request, 'restaurant_detail.html', {'restaurant': restaurant})
 
+
 def fastfood_list(request):
-    fastfoods = FastFood.objects.all()
-    context = {'fastfoods': fastfoods}
+    fastfood = FastFood.objects.all()
+    context = {'fastfood': fastfood}
     return render(request, 'fastfood_list.html', context)
+
 
 def fastfood_detail(request, pk):
     fastfood = get_object_or_404(FastFood, pk=pk)
-    return render(request, 'fastfood_detail.html', {'fastfood': fastfood})
+    context = {'fastfood': fastfood}
+    return render(request, 'fastfood_detail.html', context)
+
 
 def caffeine_list(request):
     caffeine = Caffeine.objects.all()
     context = {'caffeine': caffeine}
     return render(request, 'caffeine_list.html', context)
 
+
 def caffeine_detail(request, pk):
     caffeine = get_object_or_404(Caffeine, pk=pk)
     context = {'caffeine': caffeine}
     return render(request, 'caffeine_detail.html', context)
+
+
+
--- myproject/urls.py
+++ myproject/urls.py
@@ -6,10 +6,10 @@ from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('restaurants/', views.restaurant_list, name='restaurant_list'),
-    path('restaurants/<int:pk>/', views.restaurant_detail, name='restaurant_detail'),
-    path('fastfoods/', views.fastfood_list, name='fastfood_list'),
-    path('fastfoods/<int:pk>/', views.fastfood_detail, name='fastfood_detail'),
+    path('restaurant/', views.restaurant_list, name='restaurant_list'),
+    path('restaurant/<int:pk>/', views.restaurant_detail, name='restaurant_detail'),
+    path('fastfood/', views.fastfood_list, name='fastfood_list'),
+    path('fastfood/<int:pk>/', views.fastfood_detail, name='fastfood_detail'),
     path('caffeine/', views.caffeine_list, name='caffeine_list'),
     path('caffeine/<int:pk>/', views.caffeine_detail, name='caffeine_detail'),
 ]
